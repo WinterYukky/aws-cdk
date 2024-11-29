20615a0110bf9f0fae9b221f2a2fbea3ad83b16a
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2024-11-29 02:30:40 +0000

    WIP: implement Route53 Resolver Endpoint L2

--- packages/@aws-cdk/aws-route53resolver-alpha/lib/index.ts
+++ packages/@aws-cdk/aws-route53resolver-alpha/lib/index.ts
@@ -1,5 +1,8 @@
 export * from './firewall-domain-list';
 export * from './firewall-rule-group';
 export * from './firewall-rule-group-association';
+export * from './protocol';
+export * from './resolver-endpoint';
+export * from './resolver-rule';
 
 // AWS::Route53Resolver CloudFormation Resources:
